6a986697cddcce69bb2ed41398db72ea7104c3f5
sheyifan <1506870670@qq.com>
2020-10-23 22:41:40 +0800

    new project 解决的问题： 	1. 使用Electron加载已经完成制作的网页 	  直接从github上下载了quick start，在其中放入一套网页。这样就可以成功运行 	2. 主进程和渲染进程之间的通信 	  nodeIntegration关闭的情况下使用preload.js进行通信。如果nodeIntegrat 	-ion=true，可以直接使用renderer.js。 	  通过BrowserWindow的preload选项指定preload.js文件的位置。renderer.js 	作为外源js引入到HTML文件当中即可。 	  如果nodeIntegration=true，那么Jquery等库不能正常使用。

---
+++ preload.js
@@ -0,0 +1,22 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
+
+// 2020-10-23 sheyifan issue: communicate between main and render process
+const { ipcRenderer } = require('electron')
+console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  console.log(arg) // prints "pong"
+})
+ipcRenderer.send('asynchronous-message', 'ping')
+// 2020-10-23 sheyifan issue: communicate between main and render process
